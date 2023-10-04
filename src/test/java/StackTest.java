79675482119d6f1b5351bc8d6d352bcbf84f5c43
Antonio Dias <addsjunior@gmail.com>
2023-10-04 11:46:54 -0300

    Teste de pilha

---
+++ src/test/java/StackTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Test;
+import stack.Pilha;
+import stack.Stack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StackTest {
+
+    @Test
+    void testPushPeek() {
+        Stack stack = new Pilha();
+        assertNull(stack.peek());
+        assertTrue(stack.isEmpty());
+        stack.push("a");
+        assertEquals("a", stack.peek());
+        stack.push("b");
+        assertEquals("b", stack.peek());
+        stack.push("c");
+        assertEquals("c", stack.peek());
+        stack.push("d");
+        assertEquals("d", stack.peek());
+        stack.push("e");
+        assertEquals("e", stack.peek());
+        stack.push("f");
+        assertEquals("f", stack.peek());
+        stack.push("g");
+        assertEquals("g", stack.peek());
+        stack.push("h");
+        assertEquals("h", stack.peek());
+        stack.push("i");
+        assertEquals("i", stack.peek());
+        stack.push("j");
+        assertEquals("j", stack.peek());
+        stack.push("k");
+        assertEquals("j", stack.peek());
+        stack.push("l");
+        assertEquals("j", stack.peek());
+        assertTrue(stack.isFull());
+    }
+
+    @Test
+    void testPushPop() {
+        Stack stack = new Pilha();
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+        assertEquals("c", stack.pop());
+        assertEquals("b", stack.peek());
+        stack.push("d");
+        stack.push("e");
+        assertEquals("e", stack.pop());
+        assertEquals("d", stack.pop());
+        assertEquals("b", stack.pop());
+        assertEquals("a", stack.peek());
+        assertEquals("a", stack.pop());
+        assertNull(stack.peek());
+    }
+}
